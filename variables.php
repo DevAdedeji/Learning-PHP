1ea12b5bb3597d1ddca02a95888d448a51893561
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-23 21:52:57 +0100

    Learnt variables

--- variables.php
+++ variables.php
@@ -1,1 +1,24 @@
 <?php
+// String
+$name = 'Adedeji';
+// Int
+$age = 30;
+// Boolean
+$has_kids = false;
+// Floats
+$cash_on_hand = 20.75;
+
+// var_dump($cash_on_hand);
+
+// echo $name . ' is ' . $age . ' years old';
+// echo "${name} is ${age} years old";
+// echo 5 + 5;
+// echo 10 / 2;
+// echo 10 % 3;
+
+// Constants 
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
