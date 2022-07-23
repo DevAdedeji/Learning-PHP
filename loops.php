20c7563d08749fb0bb66c111b54712082b24a042
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-24 00:19:18 +0100

    Done with loops

---
+++ loops.php
@@ -0,0 +1,40 @@
+<?php
+// For loop
+
+// for ($s = 0; $s <= 10; $s++) {
+//     echo 'Number' . $s . '<br>';
+// }
+// While loop
+
+// $x = 1;
+// while ($x <= 15) {
+//     echo 'Number ' . $x . '<br>';
+//     $x++;
+// }
+
+// ForEach
+$posts = ['First Post', 'Second Post', 'Third Post'];
+
+// for ($x = 0; $x < count($posts); $x++) {
+//     echo 'My ' . $posts[$x] . '<br>';
+// }
+// foreach ($posts as $index => $post) {
+//     echo $index . '---' . $post . '<br>';
+// }
+
+// $people = [[
+//     'first_name' => 'Brad',
+//     'last_name' => 'Traversy',
+//     'email' => 'brad@gmail.com'
+// ], [
+//     'first_name' => 'Adedeji',
+//     'last_name' => 'Tewogbade',
+//     'email' => 'adedejitewogbade2@gmail.com'
+// ]];
+
+
+// foreach ($people as $person) {
+//     foreach ($person as $key => $value) {
+//         echo "$key -- $value <br>";
+//     }
+// }
