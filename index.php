d5efa08586044854564ba4f07c598c9100924471
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-23 21:44:48 +0100

    Lesson one done

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 //echo -output strings, numbers, html
-echo 'Hello worl kiloshele', 123, 4.5;
+// echo 'Hello worl kiloshele', 123, 4.5;
 //print - worlks like echo but can only take in a single argument
+// print 123;
+// print_r() - to print signle values and arrays
+// print_r(2);
+// print_r([1, 2, 3]);
+// var_dump() - returns more info like data type and length
+// var_dump(5);
+//var_export() - returns the string representation
+// var_export('How');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?= 'Part One'; ?></h1>
+</body>
+
+</html>
